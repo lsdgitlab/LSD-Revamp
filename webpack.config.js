fd4199d184874bc7c8ad15c6ae784072346d61c9
Ravindra Dhadave <110818058+lsdgitlab@users.noreply.github.com>
2023-02-03 16:06:46 +0530

    web-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,62 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const webpack = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const htmlPageNames = [
   {
-      pageName: 'index.html',
-      title: 'Lsdigital gallery'
-  }, 
+    pageName: 'index.html',
+    title: 'Lsdigital gallery',
+  },
   {
-      pageName: 'career-job-listing.html',
-      title: 'Career'
-  }, 
-  // {
-  //     pageName: 'detail-page.html',
-  //     title: 'Lsdigital gallery'
-  // }, 
-];
-let htmlFileName = htmlPageNames.map(htmlPage=> htmlPage.pageName);
-let multipleHtmlPlugins = htmlPageNames.map(name => {
+    pageName: 'career-job-listing.html',
+    title: 'Career',
+  },
+  {
+    pageName: 'ld-tv.html',
+    title: 'LDTV',
+  },
+  {
+    pageName: 'ldtv-category.html',
+    title: 'LDTV Catogory',
+  },
+  {
+    pageName: 'survey.html',
+    title: 'Survey',
+  },
+  {
+    pageName: 'career-details.html',
+    title: 'Career details',
+  },
+  {
+    pageName: 'start-your-journey-with-us.html',
+    title: 'start your journey with us',
+  },
+  {
+    pageName: 'thankyou.html',
+    title: 'Thank you',
+  },
+]
+let htmlFileName = htmlPageNames.map((htmlPage) => htmlPage.pageName)
+let multipleHtmlPlugins = htmlPageNames.map((name) => {
   return new HtmlWebpackPlugin({
-      // template: `./src/${name.pageName}`,
-      filename: `${name.pageName}`, 
-      // chunks: [`${name.pageName}`],
-      // chunks: [name.pageName.replace(/-(\w)/g, (match, c) => c.toUpperCase())],
-      title: name.title,
-      template: path.resolve(__dirname, `src/${name.pageName}`),
+    // template: `./src/${name.pageName}`,
+    filename: `${name.pageName}`,
+    // chunks: [`${name.pageName}`],
+    // chunks: [name.pageName.replace(/-(\w)/g, (match, c) => c.toUpperCase())],
+    title: name.title,
+    template: path.resolve(__dirname, `src/${name.pageName}`),
   })
-});
+})
 
 module.exports = (env, argv) => ({
   mode: argv.mode,
   devtool: argv.mode === 'development' ? 'source-map' : false,
-  entry: [
-    './src/js/app.js',
-    './src/css/sass/index.scss',
-  ],
+  entry: ['./src/js/app.js', './src/css/sass/index.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[contenthash].js',
@@ -52,11 +69,11 @@ module.exports = (env, argv) => ({
         patterns: [
           // {from: 'src/*.html', to: '[name].[ext]'},
           { from: './src/assets', to: './assets' },
-          { from: "./src/img", to: "./img" },
-          { from: "./src/video", to: "./video" },
-          { from: "./src/favicon.ico", to: "./favicon.ico" },
-          { from: 'src/images/*',to: 'images/[name].[ext]'}
-         ],
+          { from: './src/img', to: './img' },
+          { from: './src/video', to: './video' },
+          { from: './src/favicon.ico', to: './favicon.ico' },
+          { from: 'src/images/*', to: 'images/[name].[ext]' },
+        ],
       }),
     ],
   },
@@ -69,31 +86,33 @@ module.exports = (env, argv) => ({
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            only: ["./src"],
+            only: ['./src'],
             // name: '[name][ext]',
             // outputPath :'./js/'
-          }
+          },
         },
-      }, {
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-          }, {
+          },
+          {
             loader: 'css-loader',
             options: {
               url: false,
             },
-          }, {
+          },
+          {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [
-                  ['autoprefixer'],
-                ],
+                plugins: [['autoprefixer']],
               },
             },
-          }, {
+          },
+          {
             loader: 'sass-loader',
             options: {
               implementation: require('sass'),
@@ -107,31 +126,31 @@ module.exports = (env, argv) => ({
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       // filename: 'assets/app.css',
-      filename: argv.mode ? "./css/[name].[contenthash].css" : "./css/scss/[name].[contenthash].css",
+      filename: argv.mode
+        ? './css/[name].[contenthash].css'
+        : './css/scss/[name].[contenthash].css',
     }),
     new HtmlWebpackPartialsPlugin({
-      path:path.join(__dirname,'./src/header.html'),
-      location:'header',
-      template_filename: htmlFileName
+      path: path.join(__dirname, './src/header.html'),
+      location: 'header',
+      template_filename: htmlFileName,
     }),
     new HtmlWebpackPartialsPlugin({
-        path:path.join(__dirname,'./src/footer.html'),
-        location:'footer',
-        template_filename: htmlFileName
+      path: path.join(__dirname, './src/footer.html'),
+      location: 'footer',
+      template_filename: htmlFileName,
     }),
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      'window.jQuery': 'jquery'
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
     }),
   ].concat(multipleHtmlPlugins),
   devServer: {
-    contentBase: [
-      path.join(__dirname, '/src'),
-    ],
+    contentBase: [path.join(__dirname, '/src')],
     watchContentBase: true,
     // compress: true,
     port: 9000,
     open: true,
   },
-});
+})
